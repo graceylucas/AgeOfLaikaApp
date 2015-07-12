1250b4e24bf6cfa1fce746d974b2f3d0d9c5cb05
mary grace lucas <gaceylucas@gmail.com>
2015-07-12 13:14:06 -0400

    V1: Converts human age into dog years

---
+++ AgeOfLaikaApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  AgeOfLaikaApp
+//
+//  Created by Mary Grace Lucas on 7/12/15.
+//  Copyright (c) 2015 Mary Grace Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // This IBAction lets users enter any number (age) into the top text field and convert it into dog years
+    
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let humanAgeFromTextField = ageTextField.text.toInt()!
+        let conversionConstant = 7
+        dogYearsLabel.text = "\(humanAgeFromTextField * conversionConstant)" + " in dog years"
+        
+        dogYearsLabel.hidden = false
+        ageTextField.resignFirstResponder()
+        ageTextField.text = ""
+        
+        
+    }
+    
+    
+
+}
+
